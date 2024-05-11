bb33553f95a3aa6f17bc356cf9d40b95dec2fdd7
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-05-11 14:45:07 -0400

    inm.

---
+++ src/app/beta-open/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from '../components/chat';
+
+function Home() {
+    return (
+        <div>
+            <h1>Simple OpenAI Chat with Next.js</h1>
+            <Chat />
+        </div>
+    );
+}
+
+export default Home;
