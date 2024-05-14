c8e1912ae6bdd1c990f44727ba8dcec8a74fe454
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-05-14 13:20:50 -0400

    inm.

--- src/app/components/Alerts.js
+++ src/app/components/Alerts.js
@@ -5,7 +5,7 @@ export default function Alerts() {
     return (
         <div>
             <div style={{ backgroundColor: 'firebrick', padding: '3px', textAlign: 'center', color: 'white', fontFamily: 'sans-serif', }}>
-                <p style={{ textDecoration: 'none' }}>Our Office Are Close Today, We will be back Wednesdays (10am eastern standard time).   <Link style={{ color: 'lightslategray', textDecoration: 'none' }} href="/">Home</Link></p>
+                <p style={{ textDecoration: 'none' }}>Our office are close today, We will be back Wednesdays (10am eastern standard time).   <Link style={{ color: 'lightslategray', textDecoration: 'none' }} href="/">Home</Link></p>
             </div>
         </div>
     )
