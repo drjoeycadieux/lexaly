1bceda4f9ba384cc55e0a2af4c7dbc35def770bd
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-05-16 19:05:58 -0400

    inm.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -14,6 +14,8 @@ export default function Page() {
                     <li><p>Lexaly AI</p></li>
                     <Link className={styles.linknav} href="/">Home</Link>
                     <Link className={styles.linknav} href="/contact/">Contact</Link>
+                    <Link className={styles.linknav} href="/openchat/">OpenChat</Link>
+                    <Link className={styles.linknav} href="/search/">Search</Link>
                 </ul>
             </div>
             <Alerts />
